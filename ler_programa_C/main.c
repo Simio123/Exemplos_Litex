545b2d5d467b0af90d1b2af22a80f48d81d64b50
Emerson Santos <134620060+Simio123@users.noreply.github.com>
2026-06-29 10:47:37 -0300

    Add files via upload

---
+++ ler_programa_C/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("\n\n");
+    printf("========================================\n");
+    printf("             Become monkey              \n");
+    printf("========================================\n");
+
+    
+    while(1) { 
+    }
+
+    return 0;
+}
---
+++ ler_programa_C/main.d
@@ -0,0 +1,25 @@
+main.o: main.c \
+ /home/emerson/litex_workspace/pythondata-software-picolibc/pythondata_software_picolibc/data/libc/include/stdio.h \
+ /home/emerson/litex_workspace/pythondata-software-picolibc/pythondata_software_picolibc/data/libc/include/sys/cdefs.h \
+ /home/emerson/litex_workspace/pythondata-software-picolibc/pythondata_software_picolibc/data/libc/include/sys/config.h \
+ /home/emerson/litex_workspace/pythondata-software-picolibc/pythondata_software_picolibc/data/libc/include/machine/ieeefp.h \
+ /home/emerson/litex_workspace/pythondata-software-picolibc/pythondata_software_picolibc/data/libc/include/sys/features.h \
+ /home/emerson/Desktop/teste/ler_programa_C/build/software/include/../libc/picolibc.h \
+ /home/emerson/litex_workspace/pythondata-software-picolibc/pythondata_software_picolibc/data/libc/include/machine/_default_types.h \
+ /usr/lib/gcc/riscv64-unknown-elf/14.2.0/include/stddef.h \
+ /usr/lib/gcc/riscv64-unknown-elf/14.2.0/include/stdarg.h \
+ /home/emerson/litex_workspace/pythondata-software-picolibc/pythondata_software_picolibc/data/libc/include/sys/lock.h \
+ /home/emerson/litex_workspace/pythondata-software-picolibc/pythondata_software_picolibc/data/libc/include/sys/_types.h \
+ /home/emerson/litex_workspace/pythondata-software-picolibc/pythondata_software_picolibc/data/libc/include/machine/_types.h
+/home/emerson/litex_workspace/pythondata-software-picolibc/pythondata_software_picolibc/data/libc/include/stdio.h:
+/home/emerson/litex_workspace/pythondata-software-picolibc/pythondata_software_picolibc/data/libc/include/sys/cdefs.h:
+/home/emerson/litex_workspace/pythondata-software-picolibc/pythondata_software_picolibc/data/libc/include/sys/config.h:
+/home/emerson/litex_workspace/pythondata-software-picolibc/pythondata_software_picolibc/data/libc/include/machine/ieeefp.h:
+/home/emerson/litex_workspace/pythondata-software-picolibc/pythondata_software_picolibc/data/libc/include/sys/features.h:
+/home/emerson/Desktop/teste/ler_programa_C/build/software/include/../libc/picolibc.h:
+/home/emerson/litex_workspace/pythondata-software-picolibc/pythondata_software_picolibc/data/libc/include/machine/_default_types.h:
+/usr/lib/gcc/riscv64-unknown-elf/14.2.0/include/stddef.h:
+/usr/lib/gcc/riscv64-unknown-elf/14.2.0/include/stdarg.h:
+/home/emerson/litex_workspace/pythondata-software-picolibc/pythondata_software_picolibc/data/libc/include/sys/lock.h:
+/home/emerson/litex_workspace/pythondata-software-picolibc/pythondata_software_picolibc/data/libc/include/sys/_types.h:
+/home/emerson/litex_workspace/pythondata-software-picolibc/pythondata_software_picolibc/data/libc/include/machine/_types.h:
